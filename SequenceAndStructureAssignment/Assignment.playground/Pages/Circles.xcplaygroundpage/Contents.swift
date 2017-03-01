80524f39128536d3b459391e70b4e3319135ff2b
Student <PKIRBZ@kenneth-bone.local>
2017-03-01 11:19:26 -0500

    finished it but i have to fix the border and the circles alignment

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Circles.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Circles.xcplaygroundpage/Contents.swift
@@ -19,24 +19,40 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-// Generate a grid
-canvas.drawShapesWithFill = false
+// make the boarder
+canvas.fillColor = Color(hue: 300, saturation: 100, brightness: 100, alpha: 100)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 450, width: 500, height: 75)
+canvas.drawRectangle(bottomLeftX: 450, bottomLeftY: 0, width: 200, height: 500)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 50)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 50, height: 500)
+
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 50, height: 50)
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 450, width: 50, height: 50)
+canvas.drawRectangle(bottomLeftX: 450, bottomLeftY: 0, width: 50, height: 50)
+canvas.drawRectangle(bottomLeftX: 450, bottomLeftY: 450, width: 50, height: 50)
+
+
+
+
+canvas.drawShapesWithFill = true
 canvas.defaultBorderWidth = 5
 
 // This loop makes a 10 rows of columns
-for x in stride(from: 25, through: 475, by: 50){
-    
+for x in stride(from: 75, through: 425, by: 55){
     // This loop makes a single column, bottom to top
-    for y in stride(from: 25, through: 475, by: 50) {
+    for y in stride(from: 75, through: 425, by: 55) {
         
         // Get some entropy (a random number, 0 through 25, but never 26)
-        let expansion = random(from: 0, toButNotIncluding: 26)
+        let expansion = random(from: 25, toButNotIncluding: 38)
         
         // Set the diameter of the circle
         let diameter = 25 + expansion
         
         // Draw the circle
+        canvas.borderColor = Color.blue
+        canvas.fillColor = Color(hue: 120, saturation: 100, brightness: 100, alpha: 100)
         canvas.drawEllipse(centreX: x, centreY: y, width: diameter, height: diameter)
+        
         
     }
 }
